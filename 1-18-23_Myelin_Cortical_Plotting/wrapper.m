60de41265a61434f335309c631dab7a4d03e57a9
Risha Surana <surana.risha@gmail.com>
2023-01-21 12:27:18 -0800

    Update wrapper.m

--- 1-18-23_Myelin_Cortical_Plotting/wrapper.m
+++ 1-18-23_Myelin_Cortical_Plotting/wrapper.m
@@ -12,37 +12,56 @@ ctxp  = getCortexPlottingVars([local '/Required_Functions/cortex_plots']);
 %%% VARIABLES THAT NEED TO BE SET
 % set path to folder containing all subjects
 subjdir = getSubFold();
-% set name of subject you want to plot
-subj    =  'TBIAD241FAN';
-% set to 1 to use the same peak value for all cortical plots, or 0 to use
-% a subject-specific value
-sameCAxis = 0;
+subdir = dir(subjdir);
+out = [local '/Outputs/'];
+addpath(out);
 
-% combine paths to get to individual subject's folder
-safp = fullfile(subjdir, subj);
+% loop through directory and try a subject if output has not been made already
+for z = 3:length(subdir)
+    % set name of subject you want to plot
+    subj = subdir(z).name;
+    disp(subj); 
+    subjOutName = [subj '.jpeg'];
+    
+   
+    cont = exist(subjOutName);
+    
+    if cont == 0
+        %%try
+            % set to 1 to use the same peak value for all cortical plots, or 0 to use
+            % a subject-specific value
+            sameCAxis = 0;
 
-% load in atlas-registered myelin
-lh = load_mgh([safp '/SPMTP1lhnew_ico7.mgh']);
-rh = load_mgh([safp '/SPMTP1rhnew_ico7.mgh']);
+            % combine paths to get to individual subject's folder
+            safp = fullfile(subjdir, subj);
 
-% combine left and right hemisphere myelin 
-Q = [lh ; rh];
+            % load in atlas-registered myelin
+            lh = load_mgh([safp '/SPMTP1lhnew_ico7.mgh']);
+            rh = load_mgh([safp '/SPMTP1rhnew_ico7.mgh']);
 
-% make cortical plot
-if sameCAxis
-    % we want the same color axis peak for each subject
-    m = 1.4;
-    n = 0.9;
-else
-    % each plot will use the 90th percentile myelin value as the peak
-    m = quantile(Q,0.90);
-    % and the 10th percentile myelin value as the nadir
-    n = quantile(Q,0.1);
+            % combine left and right hemisphere myelin 
+            Q = [lh ; rh];
+
+            % make cortical plot
+            if sameCAxis
+                % we want the same color axis peak for each subject
+                m = 1.4;
+                n = 0.9;
+            else
+                % each plot will use the 90th percentile myelin value as the peak
+                m = quantile(Q,0.90);
+                % and the 10th percentile myelin value as the nadir
+                n = quantile(Q,0.1);
+            end
+            res = 300;    % resolution
+            surfs2load = {'white'}; %can be white, pial or inflated
+            outfile = [local '/Outputs/' subj '.jpeg']; % cortical plot file name
+            cmap.caxis = [n m]; %colormap axis
+            cmap.colormap = mycolortable();
+            makeCompleteCortexFigure_colorbar(outfile,ctxp.AllSurfs,ctxp.AllSurfFaces,...
+                ctxp.AllIndices2Plot,ctxp.AllFaces2Plot,Q,cmap,res,surfs2load);
+        %%catch
+            %%disp("Subject failed")
+        %%end
+    end
 end
-res = 300;    % resolution
-surfs2load = {'white'}; %can be white, pial or inflated
-outfile = [local '/Outputs/' subj '.jpeg']; % cortical plot file name
-cmap.caxis = [n m]; %colormap axis
-cmap.colormap = mycolortable();
-makeCompleteCortexFigure_colorbar(outfile,ctxp.AllSurfs,ctxp.AllSurfFaces,...
-    ctxp.AllIndices2Plot,ctxp.AllFaces2Plot,Q,cmap,res,surfs2load);
